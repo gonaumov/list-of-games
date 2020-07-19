48b2b7316fdb588e77db26dc74b8f1124fe197b0
Georgi Naumov <gonaumov@gmail.com>
2020-07-19 07:34:50 +0300

    Updates

---
+++ precache-manifest.c3a89d89ecfba6f11053d91ea993afb8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da6d780d107a63ae3abed05b976ceb36",
+    "url": "/list-of-games/index.html"
+  },
+  {
+    "revision": "a83c2e775e93b1aa8668",
+    "url": "/list-of-games/static/css/main.5c259703.chunk.css"
+  },
+  {
+    "revision": "bb5ce14a2b5001236215",
+    "url": "/list-of-games/static/js/2.94a1a315.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/list-of-games/static/js/2.94a1a315.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a83c2e775e93b1aa8668",
+    "url": "/list-of-games/static/js/main.a79c3402.chunk.js"
+  },
+  {
+    "revision": "b0815b40ac66dcc9521a",
+    "url": "/list-of-games/static/js/runtime-main.290993e8.js"
+  }
+]);
